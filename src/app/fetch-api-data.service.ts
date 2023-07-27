f41e62c6851cf47dff3ed52223e9dc6617582c1e
zabokaa <>
2023-07-27 12:17:04 +0200

    first commit

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchApiDataService {
+
+  constructor() { }
+}
