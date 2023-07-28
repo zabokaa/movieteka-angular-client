54b646d61f9786296019281fbd57fd1912d862e5
zabokaa <>
2023-07-28 20:43:09 +0200

    Added all other APIs

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,7 +8,7 @@ const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class fetchAPIdataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
@@ -21,15 +21,180 @@ export class UserRegistrationService {
     );
   }
 
-private handleError(error: HttpErrorResponse): any {
-    if (error.error instanceof ErrorEvent) {
-    console.error('Some error occurred:', error.error.message);
-    } else {
-    console.error(
-        `Error Status code ${error.status}, ` +
-        `Error body is: ${error.error}`);
-    }
-    return throwError(
-    'Something bad happened; please try again later.');
+
+
+// API for user login 
+public userLogin(userDetails: any): Observable<any> {
+  console.log(userDetails);
+  return this.http.post(apiUrl + 'login', userDetails).pipe(
+    catchError(this.handleError)
+  );
+}
+
+// API for get all movies 
+getAllMovies(): Observable<any> {
+  const token = localStorage.getItem('token');
+  return this.http.get(apiUrl + 'movies', {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+// API get 1 movie
+getOneMovie(title: string): Observable<any> {
+  const token = localStorage.getItem('token');
+  return this.http.get(apiUrl + 'movies/' + title, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+
+// API get director
+getOneDirector(directorName: string): Observable<any> {
+  const token = localStorage.getItem('token');
+  return this.http.get(apiUrl + 'movies/director/' + directorName, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+// API for get genre
+getOneGenre(genreName: string): Observable<any> {
+  const token = localStorage.getItem('token');
+  return this.http.get(apiUrl + 'movies/genre/' + genreName, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+
+// API get user
+getOneUser(): Observable<any> {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  return user;
+
+// API get favMovies
+getFavoriteMovies(): Observable<any> {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const token = localStorage.getItem('token');
+  return this.http.get(apiUrl + 'users/' + user.Username, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    map(this.extractResponseData),
+    map((data) => data.FavoriteMovies),
+    catchError(this.handleError)
+  );
+}
+// API add movie to favs
+addFavoriteMovie(movieId: string): Observable<any> {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const token = localStorage.getItem('token');
+  user.FavoriteMovies.push(movieId);
+  localStorage.setItem('user', JSON.stringify(user));
+  return this.http.post(apiUrl + 'users/' + user.Username + '/movies/' + movieId, {}, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      }),
+    responseType: "text"
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+
+isFavoriteMovie(movieId: string): boolean {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  return user.FavoriteMovies.indexOf(movieId) >= 0;
+}
+
+
+// API edit user
+editUser(updatedUser: any): Observable<any> {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const token = localStorage.getItem('token');
+  return this.http.put(apiUrl + 'users/' + user.Username, updatedUser, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+// API del user
+deleteUser(): Observable<any> {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const token = localStorage.getItem('token');
+  return this.http.delete(apiUrl + 'users/' + user._id, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      })
+  }).pipe(
+    catchError(this.handleError)
+  );
+}
+
+
+// API del movie from favs array
+deleteFavoriteMovie(movieId: string): Observable<any> {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  const token = localStorage.getItem('token');
+
+  const index = user.FavoriteMovies.indexOf(movieId);
+  console.log(index);
+  if (index > -1) { // only splice array when item is found
+    user.FavoriteMovies.splice(index, 1); // 2nd parameter means remove one item only
   }
+  localStorage.setItem('user', JSON.stringify(user));
+  return this.http.delete(apiUrl + 'users/' + user.Username + '/movies/' + movieId, {
+    headers: new HttpHeaders(
+      {
+        Authorization: 'Bearer ' + token,
+      }),
+    responseType: "text"
+  }).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+// Non-typed response extraction
+private extractResponseData(res: any): any {
+  const body = res;
+  return body || {};
+}
+// handle Error on end
+private handleError(error: HttpErrorResponse): any {
+  if (error.error instanceof ErrorEvent) {
+  console.error('Some error occurred:', error.error.message);
+  } else {
+  console.error(
+      `Error Status code ${error.status}, ` +
+      `Error body is: ${error.error}`);
+  }
+  return throwError(
+  'Something bad happened; please try again later.');
+}
 }
