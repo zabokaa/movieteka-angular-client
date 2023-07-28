21c819458098761c6a3764f8421de5950470f395
zabokaa <>
2023-07-28 20:49:24 +0200

    Need to add myAPIurl

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -4,7 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';   //have to check again !!
 @Injectable({
   providedIn: 'root'
 })
