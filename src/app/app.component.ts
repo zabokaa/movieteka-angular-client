1788bd148a44051a96920d45300ea2228181a38d
zabokaa <>
2023-07-31 10:53:16 +0200

    Updated app.comp.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  valueForm = new FormGroup({
+    input: new FormControl(),            // declaring the form fields, separated by ","
+    username: new FormControl()
+  });
   title = 'movieteka-angular-client';
 }
