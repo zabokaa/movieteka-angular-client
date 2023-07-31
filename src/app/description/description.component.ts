8bb5633cb05c89b8411c26f9ad2ef374ca6abf42
zabokaa <>
2023-07-31 18:13:41 +0200

    Updated description

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -8,6 +8,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DescriptionComponent implements OnInit {
   constructor(
+    @Inject(MAT_DIALOG_DATA)
     public data: {
       title: string,
       description: string,
